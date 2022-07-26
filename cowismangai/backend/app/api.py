c65ffc9cc1fe08fe7062a00072d182aa2b8ede2b
pedodoge <lu.jiaxin.jacky@dhs.sg>
2022-07-27 00:14:45 +0800

    removed comments

--- cowismangai/backend/app/api.py
+++ cowismangai/backend/app/api.py
@@ -25,8 +25,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True
-    # allow_methods=["*"],
-    # allow_headers=["*"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 dic = {"Ticker": "temp"}
